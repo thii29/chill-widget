0d4c6a37b5f3d7f6810ee5ece6fc3e0c6d6072a4
shirley.pham <thipham1123@gmail.com>
2026-03-25 21:59:29 +0700

    250326: add matcha cat img

--- src/components/pomodoro.tsx
+++ src/components/pomodoro.tsx
@@ -1,12 +1,27 @@
-import Image from 'next/image'
-import settingIcon from '@/src/assets/setting.png'
+import Image from 'next/image';
+import matchaCat from '@/src/assets/matcha-cat.svg';
 
 const Pomodoro = () => {
+  const isBreakTime: boolean = true;
   return (
     <div className="flex flex-col gap-4 justify-center bg-warm-200 ">
-      <button>
-        <Image src={settingIcon} alt="setting-btn" width={32} height={32}></Image>
-      </button>
+      <div className="flex justify-between font-sans text-2xl">
+        {isBreakTime ? (
+          <div className="flex justify-center items-center gap-2">
+            {' '}
+            <Image
+              src={matchaCat}
+              alt="button icon"
+              width={64}
+              height={64}
+            />{' '}
+            Break Time
+          </div>
+        ) : (
+          <div>Pomodoro</div>
+        )}
+        <button>Setting</button>
+      </div>
       <span className="px-10 py-6 border-2 border-skin-600 rounded-3xl text-9xl font-mono font-semibold ">
         25:00
       </span>
