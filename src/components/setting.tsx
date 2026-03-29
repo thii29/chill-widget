297e8273c612b1d7c1465046d337f2328e0864d2
shirley.pham <thipham1123@gmail.com>
2026-03-29 21:50:02 +0700

    290326: overview setting modal

--- src/components/setting.tsx
+++ src/components/setting.tsx
@@ -10,7 +10,7 @@ const Setting = ({ isOpen, onClose }: Props) => {
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
-      <div className="w-2xl flex flex-col gap-2.5 px-6 py-4 rounded-xl bg-ice-white text-midnight text-lg">
+      <div className="w-2xl flex flex-col gap-2.5 px-6 py-4 rounded-xl bg-deep-slate text-glacier text-lg">
         <div className="w-full flex justify-end mb-2">
           <button
             onClick={onClose}
@@ -20,7 +20,7 @@ const Setting = ({ isOpen, onClose }: Props) => {
           </button>
         </div>
         {/* Set up time */}
-        <div className="grid grid-cols-4 gap-4 my-2">
+        <div className="grid grid-cols-3 gap-4 my-2">
           <div className="font-sans">
             <div className="flex flex-col justify-start gap-1">
               <span>Sessions</span>
@@ -54,6 +54,8 @@ const Setting = ({ isOpen, onClose }: Props) => {
               />
             </div>
           </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4 my-2">
           <div className="font-sans">
             <div className="flex flex-col justify-start gap-1">
               <span>Long break</span>
@@ -65,19 +67,44 @@ const Setting = ({ isOpen, onClose }: Props) => {
               />
             </div>
           </div>
+          <div className="font-sans">
+            <div className="flex flex-col justify-start gap-1">
+              <span>Long break interval</span>
+              <input
+                type="number"
+                name="long-break-interval"
+                id=""
+                className="border rounded-md"
+              />
+            </div>
+          </div>
         </div>
         {/* Custom options */}
         <div className="flex flex-col gap-2.5">
           <div className="w-full flex justify-between items-center">
-            <span>Auto start break</span>
+            <span>Auto-start Breaks</span>
             <input
               type="checkbox"
               defaultChecked
-              className="toggle toggle-lg toggle-neutral"
+              className="toggle toggle-lg border border-midnight text-pale-laveder"
             />
           </div>
-          <div></div>
-          <div></div>
+          <div className="w-full flex justify-between items-center">
+            <span>Auto-start Focus</span>
+            <input
+              type="checkbox"
+              defaultChecked
+              className="toggle toggle-lg border border-midnight text-pale-laveder"
+            />
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <span>Auto-resume Cycle</span>
+            <input
+              type="checkbox"
+              defaultChecked
+              className="toggle toggle-lg border border-midnight text-pale-laveder"
+            />
+          </div>
         </div>
       </div>
     </div>
